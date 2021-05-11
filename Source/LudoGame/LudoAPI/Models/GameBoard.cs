48b3355ab2f049b28ac2b310c43445c36bc740e9
coachvredin <43240053+coachvredin@users.noreply.github.com>
2021-05-11 15:15:22 +0200

    Added Player & GameBoard models + dependency between projects.

---
+++ Source/LudoGame/LudoAPI/Models/GameBoard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoAPI.Models
+{
+    public class GameBoard
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+        public List<Player> Player { get; set; }
+    }
+}
