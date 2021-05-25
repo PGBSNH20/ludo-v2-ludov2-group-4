ef93671a0bf0c77acf4be4ba7c0681ae96f5707a
KevinKallin <kevin.kallin@hotmail.se>
2021-05-25 09:22:51 +0200

    Added GET request for load game

--- Source/LudoGame/LudoRazor/Pages/LoadGame/Index.cshtml.cs
+++ Source/LudoGame/LudoRazor/Pages/LoadGame/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -19,23 +22,31 @@ namespace LudoRazor.Pages.LoadGame
         {
             _dbContext = dbContext;
         }
-
+        [BindProperty(SupportsGet = true)]
         public IList<GameBoard> GameBoard { get; set; }
             
 
-        public IActionResult OnGet()
+        public async Task OnGetAsync()
         {
-            var client = new RestClient("https://localhost:44370/api/Game/get-gameboards");
-            var request = new RestRequest(Method.GET);
-            request.AddJsonBody(GameBoard);
-            var response = client.Execute<List<GameBoard>>(request);
+            //var client = new RestClient("https://localhost:44370/api/Game/get-gameboards");
+            //var request = new RestRequest(Method.GET);
+            
+            //var response = client.Execute<List<GameBoard>>(request);
+            var client = new RestClient("https://localhost:44370");
+            var request = new RestRequest("api/game/get-gameboards/", Method.GET);
+            var queryResult = client.Execute<IList<GameBoard>>(request).Data;
 
-            GameBoard = response.Data.ToList();
+            GameBoard = queryResult;
+            
+            //GameBoard = response.Data;
 
-            //Following code works, GETs GameBoards straight from Db: 
-            //GameBoard =  _dbContext.GameBoards.ToList();
+            //Following code works, GETs GameBoards straight from Db:
+            /*GameBoard = _dbContext.GameBoards.ToList();*/
 
-            return Page();
+            
+
+
         }
     }
+    
 }
